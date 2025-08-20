891cd53ebc2be7df72c53b5b01a40dd4ca6ed4f7
Wairimu2018 <wairimukuria.wk@gmail.com>
2025-08-20 18:44:11 +0300

    Removing Consulation form from homepage

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,7 @@ import Navigation from '@/components/Navigation';
 import Hero from '@/components/Hero';
 import Solutions from '@/components/Solutions';
 import Contact from '@/components/Contact';
-import ConsultationForm from '@/components/ConsultationForm';
+// import ConsultationForm from '@/components/ConsultationForm';
 import { Toaster } from '@/components/ui/toaster';
 
 const Index = () => {
@@ -13,11 +13,11 @@ const Index = () => {
       <Solutions />
       <div id="contact">
         <Contact />
-      </div>
+      {/* </div>
       <div id="consultation" className="py-16 bg-muted/30">
         <div className="container mx-auto px-4">
           <ConsultationForm />
-        </div>
+        </div> */}
       </div>
       <Toaster />
     </div>
